9db0b76539c8928da8dfe4f0a761f6b6d2cfbcfc
TobiasKozel <t.kozel@protonmail.com>
2021-03-27 15:37:05 +0100

    flecs

--- external/update.sh
+++ external/update.sh
@@ -4,11 +4,19 @@ echo "Updating rtaudio"
 rm -rf ./rtaudio
 rm ./master
 wget https://codeload.github.com/thestk/rtaudio/zip/refs/heads/master
-unzip ./master
+unzip ./master > /dev/null 2>&1
 rm ./master
 mv ./rtaudio-master ./rtaudio
 
 echo "Updaint httplib"
 rm ./httplib.h
 wget https://raw.githubusercontent.com/yhirose/cpp-httplib/master/httplib.h
+
+echo "Updating flecs"
+rm -rf ./flecs
+rm ./master
+wget https://codeload.github.com/SanderMertens/flecs/zip/refs/heads/master
+unzip ./master > /dev/null 2>&1
+rm ./master
+mv ./flecs-master ./flecs
 
