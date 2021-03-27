1abc55a2d455e063febf780c5a75f56996f06880
Tobias Kozel <t.kozel@protonmail.com>
2021-03-27 17:29:17 +0100

    remove tklb

--- external/tklb/tests/benchmarks/all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for f in ./*.cpp
-do
-	./run.sh $f
-done
-
-./BenchCompileTime.sh
--- external/tklb/util/TPrint.h
+++
@@ -1,6 +0,0 @@
-#ifndef TKLB_PRINT
-
-#include <stdio.h>
-#define TKLB_PRINT(...) printf(__VA_ARGS__);
-
-#endif // TKLB_PRINT
