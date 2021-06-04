d6f48347dc23ef0e20e47470b949530a6e637c3f
Tobias Kozel <t.kozel@protonmail.com>
2021-06-04 23:30:20 +0200

    m

---
+++ src/core/vae_engine.hpp
@@ -0,0 +1,10 @@
+#ifndef VAE_ENGINE
+#define VAE_ENGINE
+
+namespace vae { namespace core {
+	class Engine {
+		size_t mStreamTime = 0; // Global time in samples
+	};
+} } // namespace vae::core
+
+#endif // VAE_ENGINE
