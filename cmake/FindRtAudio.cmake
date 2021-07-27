baa48cb4d4cc12e269c6600258cec694079f6e80
TobiasKozel <t.kozel@protonmail.com>
2021-07-28 00:38:28 +0200

    cmake changes

--- cmake/FindRtAudio.cmake
+++ cmake/FindRtAudio.cmake
@@ -1,18 +1,13 @@
 # set(RTAUDIO_API_ASIO true)
-# set(RTAUDIO_BUILD_SHARED_LIBS BUILD_SHARED_LIBS)
-# set(BUILD_SHARED_LIBS false)
-# set(BUILD_SHARED_LIBS RTAUDIO_BUILD_SHARED_LIBS)
 
-set(RTAUDIO_BUILD_TESTING false)
+set(RTAUDIO_BUILD_TESTING OFF)
 set(RTAUDIO_BUILD_STATIC_LIBS)
-set(RTAUDIO_API_JACK false)
-set(RTAUDIO_API_ALSA false)
+set(RTAUDIO_API_JACK OFF)
+set(RTAUDIO_API_ALSA OFF)
 add_subdirectory(external/rtaudio)
 set(RtAudio_FOUND true)
 
 set(RtAudio_INCLUDE_DIRS
 	external/rtaudio
-	external/rtaudio/include)
-
-	# set(RtAudio_LIBRARIES
-	# )
+	external/rtaudio/include
+)
