cbc49669cdf5949157e512cf496fe48c3b0d97e2
Tobias Kozel <t.kozel@protonmail.com>
2021-11-25 02:52:31 +0100

    fix build with new portaudio version

--- cmake/FindRtAudio.cmake
+++ cmake/FindRtAudio.cmake
@@ -3,6 +3,16 @@ set(RTAUDIO_BUILD_TESTING off)
 set(RTAUDIO_API_JACK off)
 set(RTAUDIO_API_ALSA off)
 
+if (VAE_BUILD_STATIC_LIBS)
+	set(RTAUDIO_BUILD_STATIC_LIBS ON)
+	set(RTAUDIO_STATIC_MSVCRT OFF)
+else()
+	set(RTAUDIO_BUILD_STATIC_LIBS OFF)
+	set(RTAUDIO_STATIC_MSVCRT OFF)
+endif()
+
+
+
 add_subdirectory(external/rtaudio)
 
 set(RtAudio_FOUND true)
--- cmake/Findportaudio.cmake
+++ cmake/Findportaudio.cmake
@@ -1,6 +1,14 @@
 enable_language(C)
-set(PA_BUILD_SHARED OFF)
-set(PA_BUILD_STATIC ON)
+
+# if (VAE_BUILD_STATIC_LIBS)
+# 	set(PA_BUILD_SHARED OFF)
+# 	set(PA_BUILD_STATIC ON)
+# else()
+# 	set(PA_BUILD_SHARED ON)
+# 	set(PA_BUILD_STATIC OFF)
+# endif()
+
 set(PA_USE_ASIO OFF)
-set(PA_DISABLE_INSTALL OFF)
-add_subdirectory("external/portaudio")
+add_subdirectory(external/portaudio)
+
+set(PortAudio_FOUND true)
